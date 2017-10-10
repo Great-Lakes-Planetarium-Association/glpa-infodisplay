affee353df9bbe424f5a05891743b67f12c22a2d
Tom Dobes <tdobes@glpa-nodecg.colo.lcl>
2017-10-10 09:47:24 -0500

    adjust weather formatting

--- graphics/scripts/clock.js
+++ graphics/scripts/clock.js
@@ -1,6 +1,7 @@
 // Function calculates the current date time information and sets the appropriate div.
 var dateformat = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
 var timeformat = { hour: 'numeric', minute: '2-digit', second: '2-digit'}
+var shorttimeformat = { hour: 'numeric', minute: '2-digit' }
 function getDateTime() {
     var now = new Date();
     document.getElementById('clock').innerHTML = 

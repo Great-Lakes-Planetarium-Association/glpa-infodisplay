748114883c0edb21c8a968965ea20ed7a6ca48d1
Steven Sumichrast <s.sumichrast@mcpstars.org>
2017-10-11 04:43:44 -0500

    Let's not push twitter updates every second.

--- graphics/scripts/twitter.js
+++ graphics/scripts/twitter.js
@@ -19,7 +19,7 @@ function tweetLoop() {
         textFit(document.getElementsByClassName('tweet-text'), {minFontSize: 10, maxFontSize: 38, multiLine: true});
         textFit(document.getElementsByClassName('tweet-metadata'), {maxFontSize: 18});
         tweetIndex = (tweetIndex+1) % arrayLength;
-    }, 1000);
+    }, 10000);
 }
 
 function showTweet(index) {

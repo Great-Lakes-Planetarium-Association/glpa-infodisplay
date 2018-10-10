e18cf2f2c38c4de173db618c0828f000a703f8f9
Steven Sumichrast <steven.sumichrast@gmail.com>
2018-10-10 13:59:00 -0400

    Change twitter to use exports.

--- extension/twitter.js
+++ extension/twitter.js
@@ -1,94 +1,98 @@
-// This extension handles pulling in Tweets for display on the info page
-'use strict';
+modules.exports = function(NodeCG) {
 
-// Load twemoji module to convert emoji strings to images
-const twemoji = require('twemoji');
-// Connect to the NodeCG API because we all love replicants.
-const nodecg = require('./util/nodecg-api-context').get();
+	// This extension handles pulling in Tweets for display on the info page
+	'use strict';
 
-// Configure a Twitter object
-var Twitter = require('twitter');
-var client = new Twitter({
-	consumer_key: nodecg.bundleConfig.twitter.APIKey,
-	consumer_secret: nodecg.bundleConfig.twitter.APISecret,
-	access_token_key: nodecg.bundleConfig.twitter.AccessToken,
-	access_token_secret: nodecg.bundleConfig.twitter.AccessSecret
-  });
+	// Load twemoji module to convert emoji strings to images
+	const twemoji = require('twemoji');
 
-// Set up a replicant to track tweets in
-var tweets = nodecg.Replicant('tweets', {defaultValue: []});
-
-// getTweetCollection
-// Returns a JSON object containing tweet entries in the Twitter collection
-function getTweetCollection(ColID)
-{
-	var params = {id: "custom-" + ColID};
-	client.get('collections/entries', params, function(error, tweets, response) {
-	  if (!error) {
-			nodecg.log.info('[twitter]: Obtained updated JSON of collection from Twitter"');
-			console.log(tweets);
-			return tweets;
-	  }
+	// Configure a Twitter object
+	var Twitter = require('twitter');
+	var client = new Twitter({
+		consumer_key: nodecg.bundleConfig.twitter.APIKey,
+		consumer_secret: nodecg.bundleConfig.twitter.APISecret,
+		access_token_key: nodecg.bundleConfig.twitter.AccessToken,
+		access_token_secret: nodecg.bundleConfig.twitter.AccessSecret
 	});
-}
 
-// updateTwitterReplicant
-// This function will obtain a list of tweets from Twitter, format them, and then replace the contents of the replicant.
-function updateTwitterReplicant()
-{
-	tweets = getTweetCollection(nodecg.bundleConfig.twitter.collectionID);
-	/* Need a foreach loop here */
+	// Set up a replicant to track tweets in
+	var tweets = nodecg.Replicant('tweets', {defaultValue: []});
 
-}
-
-updateTwitterReplicant();
-
-
-/**
- * Adds a Tweet to the queue.
- * @param {Object} tweet - The tweet to add.
- * @returns {undefined}
- */
-function addTweet(tweet) {
-
-	// Don't add the tweet if we already have it
-	const isDupe = tweets.value.find(t => t.id_str === tweet.id_str);
-	if (isDupe) {
-		return;
+	// getTweetCollection
+	// Returns a JSON object containing tweet entries in the Twitter collection
+	function getTweetCollection(ColID)
+	{
+		var params = {id: "custom-" + ColID};
+		client.get('collections/entries', params, function(error, tweets, response) {
+		if (!error) {
+				nodecg.log.info('[twitter]: Obtained updated JSON of collection from Twitter"');
+				console.log(tweets);
+				return tweets;
+		}
+		});
 	}
 
-	// Parse emoji.
-	tweet.text = twemoji.parse(tweet.text);
+	// updateTwitterReplicant
+	// This function will obtain a list of tweets from Twitter, format them, and then replace the contents of the replicant.
+	function updateTwitterReplicant()
+	{
+		tweets = getTweetCollection(nodecg.bundleConfig.twitter.collectionID);
+		/* Need a foreach loop here */
 
-	// Replace newlines with spaces
-	tweet.text = tweet.text.replace(/\n/ig, ' ');
-
-	// Look for the conference hashtag in the text and replace it with a CSS class so we can style it
-	tweet.text = tweet.text.replace(RegExp(confHashTag,"g"), '<span class="hashtag">'+confHashtag+'</span>');
-
-	// Add the tweet to the list
-	tweets.value.push(tweet);
-}
-
-/**
- * Removes a Tweet (by id) from the queue.
- * @param {String} idToRemove - The ID string of the Tweet to remove.
- * @returns {Object} - The removed tweet. "Undefined" if tweet not found.
- */
-function removeTweetById(idToRemove) {
-	if (typeof idToRemove !== 'string') {
-		throw new Error(`[twitter] Must provide a string ID when removing a tweet. ID provided was: ${idToRemove}`);
 	}
 
-	let didRemoveTweet = false;
-	tweets.value.some((tweet, index) => {
-		if (tweet.id_str === idToRemove || tweet.gdqRetweetId === idToRemove) {
-			tweets.value.splice(index, 1);
-			didRemoveTweet = true;
-			return true;
+	updateTwitterReplicant();
+
+	/* Listen for a request to update Twitter */
+
+
+
+	/**
+	 * Adds a Tweet to the queue.
+	 * @param {Object} tweet - The tweet to add.
+	 * @returns {undefined}
+	 */
+	function addTweet(tweet) {
+
+		// Don't add the tweet if we already have it
+		const isDupe = tweets.value.find(t => t.id_str === tweet.id_str);
+		if (isDupe) {
+			return;
 		}
 
-		return false;
-	});
-	return didRemoveTweet;
+		// Parse emoji.
+		tweet.text = twemoji.parse(tweet.text);
+
+		// Replace newlines with spaces
+		tweet.text = tweet.text.replace(/\n/ig, ' ');
+
+		// Look for the conference hashtag in the text and replace it with a CSS class so we can style it
+		tweet.text = tweet.text.replace(RegExp(confHashTag,"g"), '<span class="hashtag">'+confHashtag+'</span>');
+
+		// Add the tweet to the list
+		tweets.value.push(tweet);
+	}
+
+	/**
+	 * Removes a Tweet (by id) from the queue.
+	 * @param {String} idToRemove - The ID string of the Tweet to remove.
+	 * @returns {Object} - The removed tweet. "Undefined" if tweet not found.
+	 */
+	function removeTweetById(idToRemove) {
+		if (typeof idToRemove !== 'string') {
+			throw new Error(`[twitter] Must provide a string ID when removing a tweet. ID provided was: ${idToRemove}`);
+		}
+
+		let didRemoveTweet = false;
+		tweets.value.some((tweet, index) => {
+			if (tweet.id_str === idToRemove || tweet.gdqRetweetId === idToRemove) {
+				tweets.value.splice(index, 1);
+				didRemoveTweet = true;
+				return true;
+			}
+
+			return false;
+		});
+		return didRemoveTweet;
+	}
 }

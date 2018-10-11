3473599c3ac84d9182060632b5889c73dd0f0f36
Steven Sumichrast <steven.sumichrast@gmail.com>
2018-10-11 17:03:39 -0400

    Export weather as a function call so NodeCG is passed in natively. Remove the 200 check -- need to fail on all errors.

--- extension/weather.js
+++ extension/weather.js
@@ -1,45 +1,45 @@
-// Load the NodeCG API
-const nodecg = require('./util/nodecg-api-context').get();
+module.exports = function (nodecg)
+{
+	// Load the Darksky API
+	const darkskyAPI = nodecg.bundleConfig.weather.APIURL + nodecg.bundleConfig.weather.APIKey + '/' + nodecg.bundleConfig.weather.location + '/?exclude=flags,alerts,hourly,minutely';
 
-// Load the Darksky API
-const darkskyAPI = nodecg.bundleConfig.weather.APIURL + nodecg.bundleConfig.weather.APIKey + '/' + nodecg.bundleConfig.weather.location + '/?exclude=flags,alerts,hourly,minutely';
+	// Specify how often to obtain weather data
+	let pollInterval = nodecg.bundleConfig.weather.interval;
 
-// Specify how often to obtain weather data
-let pollInterval = nodecg.bundleConfig.weather.interval;
+	// Check we're not refreshing faster than every 5 minutes; free API won't allow it.
+	if (pollInterval < 5) {
+		pollInterval = 5;
+	}
 
-// Check we're not refreshing faster than every 5 minutes; free API won't allow it.
-if (pollInterval < 5) {
-	pollInterval = 5;
-}
+	nodecg.log.info(`weather: Will update weather data every ${pollInterval} minutes (${pollInterval * 60 * 1000} milliseconds)`);
 
-// Obtain the weather replicant
-const weather = nodecg.Replicant('weather');
+	// Obtain the weather replicant
+	const weather = nodecg.Replicant('weather');
 
-// Load the request module
-var request = require('request');
+	// Load the request module
+	var request = require('request');
 
-console.log(`weather: Will update weather data every ${pollInterval} minutes (${pollInterval * 60 * 1000} milliseconds)`);
-
-// function updateWeather
-// Function obtains the weather data from DarkSky and populates it into a replicant for display on the graphics pages
-function updateWeather() {
-	// Make a request to grab the current weather data
-	request(darkskyAPI, function (error, response, body) {
-		// If response is okay load data into the replicant
-		if (!error && response.statusCode === 200) {
-			try {
-				weather.value = JSON.parse(body);
-				console.log('weather: New weather data received.');
-			} catch (e) {
-				console.log('weather: failed to retrieve weather data');
-				nodecg.log.error("Unable to load weather: ", e.stack);
+	// function updateWeather
+	// Function obtains the weather data from DarkSky and populates it into a replicant for display on the graphics pages
+	function updateWeather() {
+		// Make a request to grab the current weather data
+		request(darkskyAPI, function (error, response, body) {
+			// If response is okay load data into the replicant
+			if (!error) {
+				try {
+					weather.value = JSON.parse(body);
+					console.log('weather: New weather data received.');
+				} catch (e) {
+					console.log('weather: failed to retrieve weather data');
+					nodecg.log.error("Unable to load weather: ", e.stack);
+				}
 			}
-		}
-	});
-	// Request new data periodically
+		});
+		// Request new data periodically
 
-	setTimeout(updateWeather,pollInterval * 60 * 1000);
+		setTimeout(updateWeather,pollInterval * 60 * 1000);
+	}
+
+	// Start the loop
+	updateWeather();
 }
-
-// Start the loop
-updateWeather();

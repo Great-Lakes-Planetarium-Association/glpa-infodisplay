b6d04da7f6470353223938ddb6bfe0d98d4d6a2e
Steven Sumichrast <steven.sumichrast@gmail.com>
2018-10-11 09:26:02 -0400

    Bugfix for timezone.

--- graphics/scripts/clock.js
+++ graphics/scripts/clock.js
@@ -1,13 +1,16 @@
 // Get conference replicant
 conference = nodecg.Replicant("conference");
+nodecg.waitForReplicants(conference).then(() => {
+    confTimezone = conference.value.timezone;
 
-// Function calculates the current date time information and sets the appropriate div.
-function printClock() {
-    var now = new Date();
-    document.getElementById("current-time").innerHTML = `
-    <div class="clock-date">${now.toLocaleString("en-us", { "timeZone": conference.timezone, "weekday": "long", "year": "numeric", "month": "numeric", "day": "numeric" })}</div>
-    <div class="clock-time">${now.toLocaleString("en-us", { "timeZone": conference.timezone, "hour": "numeric", "minute": "2-digit","second": "2-digit"})}</div>`;
-    setTimeout(printClock, 500);
-}
+    // Function calculates the current date time information and sets the appropriate div.
+    function printClock() {
+        var now = new Date();
+        document.getElementById("current-time").innerHTML = `
+        <div class="clock-date">${now.toLocaleString("en-us", { "timeZone": confTimezone, "weekday": "long", "year": "numeric", "month": "numeric", "day": "numeric" })}</div>
+        <div class="clock-time">${now.toLocaleString("en-us", { "timeZone": confTimezone, "hour": "numeric", "minute": "2-digit","second": "2-digit"})}</div>`;
+        setTimeout(printClock, 500);
+    }
 
-printClock();
+    printClock();
+});

a4716b02fc17b1be9349777917caa11f0c61c610
Steven Sumichrast <steven.sumichrast@gmail.com>
2018-10-12 03:16:27 -0400

    remove debug log lines

--- graphics/scripts/twitter.js
+++ graphics/scripts/twitter.js
@@ -39,11 +39,8 @@ function showTweet()
     // Using the index position, look in the tweet time line for the tweet id.
     // Pass the tweet ID from the timeline into the tweets list to get the tweet
     let tweetID = tweetOrder[nextTimelineID];
-    console.log("tweetID is: " + tweetID);
     let tweet = tweetReplicant.value.objects.tweets[tweetID];
-    console.log("tweet is: " + tweet);
     let user = tweetReplicant.value.objects.users[tweet.user.id];
-    console.log("user is: " + user);
     let tweetTime = new Date(Date.parse(tweet.created_at.replace(/( \+)/, ' UTC$1'))); 
 
 

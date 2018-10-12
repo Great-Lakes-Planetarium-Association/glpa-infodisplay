60d31eba2b2c9d86935214afb10b0cbda787c66f
Steven Sumichrast <steven.sumichrast@gmail.com>
2018-10-11 21:25:01 -0400

    deprecated clock and admin scripts. Moved clock functionality into general scripts file. Moved admin functions into scripts file.

--- graphics/scripts/admin.js
+++
@@ -1,5 +0,0 @@
-// Listen for a broadcast requesting a page reload.  If so, reload the page.
-nodecg.listenFor("ReloadBrowser", message => {
-    // Force a reload of the page from the server
-    location.reload(true)
-})
--- graphics/scripts/scripts.js
+++ graphics/scripts/scripts.js
@@ -1,20 +1,31 @@
 // Create document replicants
 const conference = nodecg.Replicant('conference');
 
-
-// Call functions after a replicant loads
-NodeCG.waitForReplicants(conference).then(() => {
-
-    // Set the clock once we have timezone data
-    setClock(conference.value.timezone);
-
+// Listen for a broadcast requesting a page reload.  If so, reload the page.
+nodecg.listenFor("ReloadBrowser", message => {
+    // Force a reload of the page from the server
+    location.reload(true)
 });
 
-function setClock(timezone)
+// General things to trigger when conference data is updated
+conference.on('change', newval => {
+    console.log(`conference: Received new conference.json from live.glpa.org.  Updating content.`);
+    document.getElementById("header-title").innerText = newval.conference;
+    document.getElementById("twitter-hashtag").innerText = newval.twitter.hashtag;
+});
+
+
+// Set the clock on the page
+function setClock()
 {
-    var now = new Date();
-    document.getElementById("current-time").innerHTML = `
-    <div class="clock-date">${now.toLocaleString("en-us", { "timeZone": timezone, "weekday": "long", "year": "numeric", "month": "numeric", "day": "numeric" })}</div>
-    <div class="clock-time">${now.toLocaleString("en-us", { "timeZone": timezone, "hour": "numeric", "minute": "2-digit","second": "2-digit"})}</div>`;
-    setTimeout(setClock(timezone), 500);
+    // Requires the conference replicant
+    NodeCG.waitForReplicants(conference).then(() =>
+    {
+        let timezone = conference.value.timezone;
+        let now = new Date();
+        document.getElementById("current-time").innerHTML = `
+        <div class="clock-date">${now.toLocaleString("en-us", { "timeZone": timezone, "weekday": "long", "year": "numeric", "month": "numeric", "day": "numeric" })}</div>
+        <div class="clock-time">${now.toLocaleString("en-us", { "timeZone": timezone, "hour": "numeric", "minute": "2-digit","second": "2-digit"})}</div>`;
+        setTimeout(setClock(), 500);
+    });
 }

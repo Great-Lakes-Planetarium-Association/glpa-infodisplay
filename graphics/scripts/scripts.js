5db3c6d7e04230f1230d8b5a1b7cb70555883b5d
Steven Sumichrast <steven.sumichrast@gmail.com>
2018-10-11 22:48:53 -0400

    Change loading for clock

--- graphics/scripts/scripts.js
+++ graphics/scripts/scripts.js
@@ -14,18 +14,19 @@ conference.on('change', newval => {
     document.getElementById("twitter-hashtag").innerText = newval.twitter.hashtag;
 });
 
+// Call the setClock functio after conference replicant is loaded
+NodeCG.waitForReplicants(conference).then(() =>
+{
+    setClock();
+});
+
 // Set the clock on the page
 function setClock()
 {
-    // Requires the conference replicant
-    NodeCG.waitForReplicants(conference).then(() =>
-    {
-        let timezone = conference.value.timezone;
-        let now = new Date();
-        document.getElementById("current-time").innerHTML = `
-        <div class="clock-date">${now.toLocaleString("en-us", { "timeZone": timezone, "weekday": "long", "year": "numeric", "month": "numeric", "day": "numeric" })}</div>
-        <div class="clock-time">${now.toLocaleString("en-us", { "timeZone": timezone, "hour": "numeric", "minute": "2-digit","second": "2-digit"})}</div>`;
-        setTimeout(setClock(), 500);
-    });
+    let timezone = conference.value.timezone;
+    let now = new Date();
+    document.getElementById("current-time").innerHTML = `
+    <div class="clock-date">${now.toLocaleString("en-us", { "timeZone": timezone, "weekday": "long", "year": "numeric", "month": "numeric", "day": "numeric" })}</div>
+    <div class="clock-time">${now.toLocaleString("en-us", { "timeZone": timezone, "hour": "numeric", "minute": "2-digit","second": "2-digit"})}</div>`;
+    setTimeout(setClock(), 500);
 }
-setClock();

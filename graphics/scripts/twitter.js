e34018d4d13de16fd55a3c3edf28b77d3de8c6eb
Steven Sumichrast <steven.sumichrast@gmail.com>
2018-10-12 02:28:29 -0400

    box now on the line.

--- graphics/scripts/twitter.js
+++ graphics/scripts/twitter.js
@@ -48,8 +48,7 @@ function showTweet()
     document.getElementById('tweettime').innerText = tweetTime.toLocaleDateString("en-us", {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: '2-digit'});
     document.getElementById('tweet-text').innerHTML = tweet.full_text.replace(/https:\/\/t.co\/\S+/,'');
     document.getElementById('avatar').src = user.profile_image_url.replace('_normal',"_bigger");
-    textFit(document.getElementsByClassName('tweet-text'), {minFontSize: 10, maxFontSize: 38, multiLine: true});
-    textFit(document.getElementsByClassName('tweet-metadata'), {maxFontSize: 18});
+
     /*if (tweet.entities.media) {
         document.getElementsByClassName('twitter-content')[0].style.background = 'url(' + tweet.entities.media[0].media_url + ') no-repeat top left';
         document.getElementsByClassName('twitter-content')[0].style.backgroundSize = 'auto 100%';
@@ -62,6 +61,8 @@ function showTweet()
     } else {
         document.getElementById('tweet-media').src = null;
     }
+    textFit(document.getElementsByClassName('tweet-text'), {minFontSize: 10, maxFontSize: 38, multiLine: true});
+    textFit(document.getElementsByClassName('tweet-metadata'), {maxFontSize: 18});
 
     // Set the nextTimeLineID to the currentTimelineID
     currentTimelineID = nextTimelineID;

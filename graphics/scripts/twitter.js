6fcd2506d82ea00b4ee9eb76d389820739ccfb39
Steven Sumichrast <steven.sumichrast@gmail.com>
2018-10-20 11:35:44 -0500

    Updated Twitter extension to pass object into function properly.

--- graphics/scripts/twitter.js
+++ graphics/scripts/twitter.js
@@ -1,13 +1,12 @@
-// Listen for updates to the twitter_current_tweet replicant.
-// When change is found, display it on the page.
-
+// Pull in the current_tweet replicant.  Whenever it changes run the appropriate
+// functions to set it up and display.
 var current_tweet = nodecg.Replicant('current_tweet');
 current_tweet.on('Change', tweet =>
     {
         console.log('[twitter]: current_tweet updated; beginning update process.');
         // Fade out the tweet container, wait 1 second for fade, then set tweet
         tweet_visibility(false);
-        setTimeout(set_tweet,1000);
+        setTimeout(set_tweet(tweet),1000);
 
         // Wait one second for tweet data to load, then fade up container.
         setTimeout(tweet_visibility(true), 1000);
@@ -16,10 +15,11 @@ current_tweet.on('Change', tweet =>
 )
 
 /**
- * Set tweet on page.
- * Sets the tweet HTML elements to the appropriate values according to the current tweet data.
+ * Set tweet on page. 
+ * Sets the tweet HTML elements to the appropriate values
+ * according to the current tweet data.
  */
-function set_tweet()
+function set_tweet(tweet)
 {
     console.log('[twitter]: Setting tweet data on page.');
     document.getElementById('screenname').innerHTML = tweet.screen_name + ' &mdash; ';

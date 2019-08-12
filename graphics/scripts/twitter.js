07ac659d0765e2b2ae1dc03a0afdd8952df677dd
Steven Sumichrast <steven.sumichrast@gmail.com>
2019-08-12 11:12:45 -0500

    Removed some verbosity from the front end twitter script.

--- graphics/scripts/twitter.js
+++ graphics/scripts/twitter.js
@@ -4,14 +4,13 @@ const activeTweet = nodecg.Replicant('activeTweet');
 console.log("hello");
 
 activeTweet.on('change', tweet => {
-    console.log('[twitter]: Received new tweet from backend.');
+    console.log('[twitter]: Processing new tweet from backend.');
     // Fade out the tweet container, wait 1 second for fade, then set tweet
     tweet_visibility(false);
     setTimeout(set_tweet(tweet), 1000);
 
     // Wait one second for tweet data to load, then fade up container.
     setTimeout(tweet_visibility(true), 1000);
-    console.log('[twitter]: update to tweet complete.');
 });
 
 /**
@@ -21,7 +20,6 @@ activeTweet.on('change', tweet => {
  */
 function set_tweet(tweet)
 {
-    console.log('[twitter]: Setting tweet data on page.');
     document.getElementById('screenname').innerHTML = tweet.screen_name + ' &mdash; ';
     document.getElementById('tweettime').innerText = tweet.created_at;
     document.getElementById('tweet-text').innerHTML = tweet.formatted_text;
@@ -37,7 +35,6 @@ function set_tweet(tweet)
     }
     textFit(document.getElementsByClassName('tweet-text'), {minFontSize: 10, maxFontSize: 38, multiLine: true});
     textFit(document.getElementsByClassName('tweet-metadata'), {maxFontSize: 18});
-    console.log('[twitter]: Completed setting tweet data on page.');
 }
 
 /**
@@ -50,12 +47,10 @@ function tweet_visibility(visible)
 {
     if (visible)
     {
-        console.log('[twitter]: Showing tweet container.');
         document.getElementById('tweet-container').style.opacity = '1';
     }
     else
     {
-        console.log('[twitter]: Hiding tweet container.');
         document.getElementById('tweet-container').style.opacity = '0';
     }
 }

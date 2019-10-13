2485560c673e17d8e55b71d8fe5de4c477758d02
Steven Sumichrast <steven.sumichrast@gmail.com>
2019-10-12 21:30:46 -0500

    Significant overhaul to MVP for infodisplay UI.

--- graphics/scripts/twitter.js
+++ graphics/scripts/twitter.js
@@ -11,7 +11,7 @@ activeTweet.on('change', tweet => {
  * Hides the tweet content from the screen.
  */
 function hideTweet() {
-    document.getElementById('block-tweet').style.opacity = '0';
+    document.getElementById('block-twitter').style.opacity = '0';
     return;
 }
 
@@ -19,7 +19,7 @@ function hideTweet() {
  * Shows the tweet content on the screen.
  */
 function showTweet() {
-    document.getElementById('block-tweet').style.opacity = '1';
+    document.getElementById('block-twitter').style.opacity = '1';
     return;
 }
 
@@ -31,19 +31,20 @@ function showTweet() {
 function set_tweet(tweet) {
     hideTweet();
     setTimeout(function () {
+        document.getElementById('block-twitter').className = tweet.layout;
         document.getElementById('tweet-username').innerText = tweet.name;
         document.getElementById('tweet-screenname').innerText = tweet.screen_name;
         document.getElementById('tweet-time').innerText = tweet.created_at;
         document.getElementById('tweet-text').innerHTML = tweet.formatted_text;
-        document.getElementById('tweet-avatar').src = tweet.avatar;
+        document.getElementById('tweet-author-avatar').src = tweet.avatar;
 
         if (tweet.image) {
-            document.getElementById('tweet-media').src = tweet.image.media_url;
+            document.getElementById('tweet-image-1').src = tweet.image.media_url;
         }
         else {
-            document.getElementById('tweet-media').src = "";
+            document.getElementById('tweet-media-1').src = "";
         }
-        textFit(document.getElementsByClassName('block-twitter-tweet-text'), { minFontSize: 10, maxFontSize: 38, multiLine: true });
+        textFit(document.getElementsByClassName('textFit tweet'), { minFontSize: 10, maxFontSize: 48, multiLine: true });
         textFit(document.getElementsByClassName('block-twitter-tweet-metadata'), { maxFontSize: 18 })
     },1000);
     setTimeout(showTweet, 1000);
